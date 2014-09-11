1854340fa2a50f8df410921e80878e7337202139
刘锦 <522978593@qq.com>
2014-09-11 11:59:09 +0800

    first commit

---
+++ 微妮/微妮/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  微妮
+//
+//  Created by 刘锦 on 14-9-11.
+//  Copyright (c) 2014年 liujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
